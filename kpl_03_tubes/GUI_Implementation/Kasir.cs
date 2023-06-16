359c6c9762b4a066892c1643339bac5a17e9ec74
MrLI0N7 <93116969+MrLI0N7@users.noreply.github.com>
2023-06-16 22:06:35 +0700

    Commit PrintStruk

--- kpl_03_tubes/GUI_Implementation/Kasir.cs
+++ kpl_03_tubes/GUI_Implementation/Kasir.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Library_StatusOrder;
+using PrintStruk;
 
 
 namespace GUI_Implementation
@@ -16,10 +17,12 @@ namespace GUI_Implementation
     {
         private List<Order> orders;
         private GUIController controller;
+        private PrintStrukPDF printer;
         public Kasir()
         {
             InitializeComponent();
             controller = new GUIController();
+            printer = new PrintStrukPDF();
         }
 
         private void Kasir_Load(object sender, EventArgs e)
@@ -71,7 +74,9 @@ namespace GUI_Implementation
 
         private void buttonPrintStruk_Click(object sender, EventArgs e)
         {
-
+            //implementasi belum selesai :/
+            Receipt receipt = new Receipt();
+            printer.GenerateReceipt("D:\\", receipt);
         }
 
         private void buttonLogoutKasir_Click(object sender, EventArgs e)
---
+++ kpl_03_tubes/PrintStruk/Receipt.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MengaturMenu;
+
+namespace PrintStruk
+{
+    public class Receipt
+    {
+    
+            public string idTransaksi { get; set; }
+            public DateTime tanggal { get; set; }
+            public List<MenuMakanan<string>> items { get; set; }
+            public double totalCost { get; set; }
+
+            public Receipt() { }
+
+            public Receipt(string iD_Transaksi, DateTime tanggal, List<MenuMakanan<string>> items)
+            {
+                Contract.Requires(!string.IsNullOrWhiteSpace(iD_Transaksi), "ID_Transaksi cannot be null or empty");
+                Contract.Requires(items != null && items.Count > 0, "Items cannot be null or empty");
+                Contract.Requires(Contract.ForAll(items, item => item != null), "All Items cannot be null");
+                Contract.Requires(Contract.ForAll(items, item => item.hargaMenu > 0), "All items harga_menu should be greater than 0");
+                Contract.Requires(tanggal <= DateTime.Today, "Tanggal cannot be greater than today");
+
+                this.idTransaksi = iD_Transaksi;
+                this.tanggal = tanggal;
+                this.items = items;
+                this.totalCost = items.Sum(item => item.hargaMenu);
+            }
+    }
+}
+
